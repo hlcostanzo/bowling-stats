87258714877946b16c6a9d49bc79741f8aea2afd
Holly Costanzo <holly.costanzo@kroger.com>
2017-12-16 15:27:15 -0500

    First commit

---
+++ src/main/java/bowling/stats/bowlingstats/BowlingStatsApplication.java
@@ -0,0 +1,12 @@
+package bowling.stats.bowlingstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingStatsApplication.class, args);
+	}
+}
